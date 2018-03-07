051cb7112bba7ec2bb141516a761cb0da0d47902
Chris <chrishjimenez@gmail.com>
2018-03-06 21:35:25 -0500

    Added randomizing to the color the user must guess
    Added function that randomizes the color the user must guess to win

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ var colors = [
  ];
 
  var squares = document.querySelectorAll(".square");
- var pickedColor = colors[3];
+ var pickedColor = pickColor();
  var colorDisplay = document.getElementById("color-display");
+ var messageDisplay = document.querySelector("#message");
+
  colorDisplay.textContent = pickedColor;
 
  for(var i = 0; i < squares.length; i++){
@@ -19,10 +21,22 @@ var colors = [
    squares[i].addEventListener("click", function(){
      var clickedColor = this.style.backgroundColor;
      if(clickedColor === pickedColor){
-       alert("Correct");
-       } else {
-         alert("wrong");
+       messageDisplay.textContent = "Correct!";
+       changeColors(clickedColor);
+     } else {
+       this.style.backgroundColor = "#232323";
+       messageDisplay.textContent = "Try Again";
        }
-
    });
  };
+
+ function changeColors(color){
+   for (var i = 0; i < squares.length; i++){
+     squares[i].style.backgroundColor = color;
+   }
+ }
+
+ function pickColor(){
+   var random = Math.floor(Math.random() * colors.length);
+   return colors[random];
+ }
