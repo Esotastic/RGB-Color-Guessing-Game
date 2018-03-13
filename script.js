f5de06b4e8247f07f9d76499f564782eb15bdd6c
Chris <chrishjimenez@gmail.com>
2018-03-12 20:57:11 -0400

    Complete JS refactor
    Created function that replaces the two separate difficulty button code with a single reset(); function. Also used that function within the reset(); button.

--- script.js
+++ script.js
@@ -6,49 +6,38 @@ var colorDisplay = document.getElementById("color-display");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
-easyBtn.addEventListener("click", function(){
-  numSquares = 3;
-  easyBtn.classList.add("selected");
-  hardBtn.classList.remove("selected");
+for(var i=0; i<modeButtons.length; i++){
+  modeButtons[i].addEventListener("click", function(){
+    modeButtons[0].classList.remove("selected");
+    modeButtons[1].classList.remove("selected");
+    this.classList.add("selected");
+    this.textContent ==="Easy" ? numSquares = 3 : numSquares = 6;
+    reset();
+  });
+}
+
+function reset(){
   colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
   for (var i = 0; i < squares.length; i++){
     if(colors[i]){
+      squares[i].style.display = "block";
       squares[i].style.backgroundColor = colors[i];
     } else {
       squares[i].style.display = "none";
     }
   }
-});
+  messageDisplay.textContent = "";
+  h1.style.backgroundColor = "steelblue";
+  resetButton.textContent = "New Colors";
 
-hardBtn.addEventListener("click", function(){
-  hardBtn.classList.add("selected");
-  easyBtn.classList.remove("selected");
-  numSquares = 6;
-  colors = generateRandomColors(numSquares);
-  pickedColor = pickColor();
-  colorDisplay.textContent = pickedColor;
-  for (var i = 0; i < squares.length; i++){
-    if(colors[i]){
-      squares[i].style.backgroundColor = colors[i];
-      squares[i].style.display = "block";
-  }
- }
-});
+}
 
 resetButton.addEventListener("click", function(){
-  colors = generateRandomColors(numSquares);
-  pickedColor = pickColor();
-  colorDisplay.textContent = pickedColor;
-  for (var i = 0; i < squares.length; i++){
-    squares[i].style.backgroundColor = colors[i];
-  }
-h1.style.backgroundColor = "steelblue";
-resetButton.textContent = "New Colors";
+  reset();
 })
 
 colorDisplay.textContent = pickedColor;
